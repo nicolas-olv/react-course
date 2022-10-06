08536ec5a98e982e4ef1706bc0e47796fbea5988
Nicolas Oliveira <nicolasoliveira.02@outlook.com>
2022-10-06 13:54:51 -0300

    CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import Pessoa from "./components/Pessoa";
 import SayMyName from "./components/SayMyName";
+import Frase from "./components/Frase";
 
 function App() {
   const nome = "Maria";
 
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase></Frase>
+      <hr />
       <HelloWorld />
       <hr />
       <SayMyName nome="Nicolas" />

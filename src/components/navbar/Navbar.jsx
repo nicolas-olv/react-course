59e35c89d08a40f12e3eabfc62b0d834aed79db7
Nicolas Oliveira <nicolasoliveira.02@outlook.com>
2022-10-10 11:55:38 -0300

    React Icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaBlackTie, FaHome, FaScroll } from "react-icons/fa";
 import "./Navbar.css";
 
 function Navbar() {
@@ -7,16 +8,19 @@ function Navbar() {
     <div className="navbar">
       <ul>
         <li>
+          <FaHome className="icon" />
           <Link to="/" className="link">
             Home
           </Link>
         </li>
         <li>
+          <FaBlackTie className="icon" />
           <Link to="/empresa" className="link">
             Empresa
           </Link>
         </li>
         <li>
+          <FaScroll className="icon" />
           <Link to="/contato" className="link">
             Contato
           </Link>
